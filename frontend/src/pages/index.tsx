d8a266f38d51f90c27f0015fe61d777a46716622
Krishnamuti <61639711+Krishnamuti@users.noreply.github.com>
2022-09-18 10:07:44 -0300

    Componente Tabela

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,14 +1,35 @@
 import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
 
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 45, '2'),
+    new Cliente('Carlos', 23, '3'),
+    new Cliente('Pedro', 54, '4'),
+  ];
+
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(`Editar: ${cliente.nome}`);
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+    console.log(`Excluir: ${cliente.nome}`);
+  }
+
   return (
     <div className={`
       flex h-screen justify-center items-center
       bg-gradient-to-r from-purple-500 to-blue-600
     `}>
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>
+        <Tabela
+          clientes={clientes}
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido} />
       </Layout>
     </div>
   )
